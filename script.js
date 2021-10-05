5b38950aec2e5c6d837f9022a707575b7414c4a2
Calvin Roberts <roberts.calvin85@gmail.com>
2021-10-05 19:59:37 +0100

    Add event listener to input value

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const pass = document.getElementById('password'),
   msg = document.getElementById('message'),
   str = document.getElementById('strength');
+
+pass.addEventListener('input', () => {
+  if (pass.value.length > 0) {
+    msg.style.display = 'block';
+  } else {
+    msg.style.display = 'none';
+  }
+});
